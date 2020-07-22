079b54478c5d7d4bf61de39d01c8d07e1e66b2d5
Kadir <kadirincedev@gmail.com>
2020-07-22 17:08:22 +0300

    Initial Commit

---
+++ Todo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Todo
+//
+//  Created by Kadir on 22.07.2020.
+//  Copyright © 2020 Kadir. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
