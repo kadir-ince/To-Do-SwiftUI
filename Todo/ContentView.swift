9dbb4a78bbe7871ce5af0356a76539351f693a26
Kadir <kadirincedev@gmail.com>
2020-07-22 17:26:11 +0300

    Create Template of UI

--- Todo/ContentView.swift
+++ Todo/ContentView.swift
@@ -12,7 +12,35 @@ struct ContentView: View {
     @State private var todoItem = ""
     @State private var setPriority = ""
     var body: some View {
-        
+        VStack {
+            NavigationView {
+                List {
+                    HStack {
+                        TextField("Enter an item", text: $todoItem)
+                            .padding(.all, 2)
+                            .font(.system(size: 25, design: .default))
+                            .multilineTextAlignment(.center)
+                            .textFieldStyle(RoundedBorderTextFieldStyle())
+                        Button(action: {
+                            //
+                        }) {
+                            Image(systemName: "plus.circle")
+                                .imageScale(.large)
+                                .foregroundColor(.blue)
+                        }
+                    }
+                    Picker(selection: $setPriority, label: Text("")) {
+                        Text("Hello").tag(0)
+                        Text("Hello").tag(1)
+                        Text("Hello").tag(2)
+                    }
+                    .pickerStyle(SegmentedPickerStyle())
+                    
+                    
+                    
+                }.navigationBarTitle("Todo")
+            }
+        }.environment(\.colorScheme, .dark)
     }
 }
 
